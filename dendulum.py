37767a354154a31e83b3a468ca204e5610a0d1ca
ipchatz <52156039+ipchatz@users.noreply.github.com>
2021-05-16 16:16:57 +0100

    Implemented Damped Double Pendulum Simulation
    The double pendulum system now features two damping constants, one for each of the constituent pendula

--- dendulum.py
+++ dendulum.py
@@ -12,11 +12,12 @@ from scipy.integrate import odeint
 
 m1 = m2 = 1 #bob masses (kg)
 l1 = l2 = 1 #string lengths (m)
+k1 = k2 = 0.42 #damping constants
 g = 9.807 #gravitational acceleration (ms^-2)
 dt = 0.01 #time delta (s)
 t_end = 5 #simulation time (s)
-theta1_0 = np.pi / 6 #initial angle 1 (rad)
-theta2_0 = -np.pi / 6 #initial angle 2 (rad)
+theta1_0 = np.pi / 2 #initial angle 1 (rad)
+theta2_0 = -np.pi / 2 #initial angle 2 (rad)
 theta1 = theta1_0 #angular displacement 1 (rad)
 theta2 = theta2_0 #angular displacement 2 (rad)
 omega1_0 = 0 #intial angular velocity 1 (rads^-1)
@@ -37,6 +38,23 @@ def genGraph(x, y, title_x = "", title_y = ""): #generates a specific graph
     plt.grid() #adds gridlines to the graph
     plt.savefig("dendulum" + str(choice(T)) + ".png") #saves the figure
     plt.show() #shows the dendulum graph
+
+def simDeudulum(init, t, l1, l2, m1, m2, g): #simulates the dendulum motion
+    theta1, theta2, omega1, omega2 = init #initial state of the dendulum
+    delta = theta1 - theta2 #difference between angular displacements
+    M = m1 + m2 #total mass of the two bobs
+    alpha1_a = m2 * g * s(theta2) * c(delta) #components of the equations of motion
+    alpha1_b = - m2 * s(delta) * (l1 * (omega1**2) * c(delta) + l2 * (omega2**2))
+    alpha1_c = - M * g * s(theta1)
+    alpha1_d = l1 * (m1 + m2 * (s(delta))**2)
+    alpha2_a = M * (l1 * (omega1**2) * s(delta))
+    alpha2_b = M * g * (-s(theta2) + s(theta1) * c(delta))
+    alpha2_c = m2 * l2 * (omega2**2) * s(delta) * c(delta)
+    alpha2_d = l2 * (m1 + m2 * (s(delta))**2)
+    alpha1 = (alpha1_a + alpha1_b + alpha1_c) / alpha1_d #equations of motion
+    alpha2 = (alpha2_a + alpha2_b + alpha2_c) / alpha2_d
+    cont = [omega1, omega2, alpha1, alpha2] #the derived state of the dendulum
+    return cont #returns the first derivatives of the equations
 
 def simDendulum(init, t, l1, l2, m1, m2, g): #simulates the dendulum
     theta1, theta2, omega1, omega2 = init #initial state of the dendulum
@@ -72,7 +90,28 @@ def simChendulum(init, t, l1, l2, m1, m2, g): #simulates the dendulum motion
     cont = [omega1, omega2, alpha1, alpha2] #the derived state of the dendulum
     return cont #returns the first derivatives of the equations
 
-sol = odeint(simChendulum, inits, T, (l1, l2, m1, m2, g)) #solve equation
+def simDDendulum(init, t, l1, l2, m1, m2, g, k1, k2): #simulates the dendulum motion
+    theta1, theta2, omega1, omega2 = init #initial state of the dendulum
+    delta = theta1 - theta2 #difference between angular displacements
+    M = m1 + m2 #total mass of the two bobs
+    alpha1_a = m2 * l1 * (omega1**2) * s(2 * delta) #equation components
+    alpha1_b = 2 * m2 * l2 * (omega2**2) * s(delta)
+    alpha1_c = 2 * m2 * g * c(theta2) * s(delta) + 2 * m1 * g * s(theta1)
+    alpha1_d = 2 * k1 * omega1 - 2 * k2 * omega2 * c(delta)
+    alpha1_e = -2 * l1 * (m1 + m2 * (s(delta))**2)
+    alpha2_a = m2 * l2 * (omega2**2) * s(2 * delta)
+    alpha2_b = 2 * M * l1 * (omega1**2) * s(delta)
+    alpha2_c = 2 * M * g * c(theta1) * s(delta)
+    alpha2_d = 2 * k1 * (omega1) * c(delta)
+    alpha2_e = -2 * M * k2 * omega2 / m2
+    alpha2_f = 2 * l2 * (m1 + m2 * (s(delta))**2)
+    alpha1 = (alpha1_a + alpha1_b + alpha1_c + alpha1_d) / alpha1_e #equations
+    alpha2 = (alpha2_a + alpha2_b + alpha2_c + alpha2_d + alpha2_e) / alpha2_f
+    cont = [omega1, omega2, alpha1, alpha2] #the derived state of the dendulum
+    return cont #returns the first derivatives of the equations
+
+sol = odeint(simDDendulum, inits, T, (l1, l2, m1, m2, g, k1, k2)) #solve
+#sol = odeint(simDendulum, inits, T, (l1, l2, m1, m2, g)) #solve equations
 
 theta1 = sol[:, 0] #obtains the theta values from the solution
 theta2 = sol[:, 1] #obtains the theta values from the solution
